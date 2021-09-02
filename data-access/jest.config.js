d2d29e5fb172ba49c552a5fb850504f49d0e2b00
Patrick Gidich <patrick.gidich@simnova.com>
2021-09-02 17:40:57 -0400

    tests-2 (#11)
    * tests
    
    * tests
    
    * ci integration for tests
    
    * fixing dependencies
    
    * coverage
    
    * fixing coverage report
    
    * setting root
    
    * yml
    
    * tweaking descriptions
    
    * removing function tools, may not be needed
    
    * cleanup
    
    * functions yml
    
    * jest tweaks
    
    * code checkin
    
    * ci
    
    * npm cache
    
    * removing npx
    
    * uugh
    
    * tweak
    
    * remove npx
    
    * install ts-jest forcefully
    
    * run tests before build
    
    * cache npm even for PRs
    
    * updated task names
    
    * xx
    
    Co-authored-by: Patrick Gidich <patrick@simnova.com>

--- data-access/jest.config.js
+++ data-access/jest.config.js
@@ -1,23 +1,21 @@
-// jest.config.js
-
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-    roots: ["<rootDir>/."],
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/**/*.test.ts'],
+  roots: ["<rootDir>/."],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/**/*.test.ts'],
     coverageReporters: ['json', 'lcov'],
     coverageDirectory: 'coverage',
     collectCoverageFrom: [
         "**/*.{js,jsx,ts}",
         "!**/node_modules/**",
-        "!**/vendor/**",
         "!**/dist/**"
       ]
-}
+};
 
 process.env = Object.assign(process.env, {    
-    APPINSIGHTS_INSTRUMENTATIONKEY: "95c3d0e3-d463-4a8d-a496-f22e9806bb8b",
-    WEBSITE_HOSTNAME: "localhost",
-    BASIC_AUTH_USERNAME: "testUser",
-    BASIC_AUTH_PASSWORD: "testPassword",
+  APPINSIGHTS_INSTRUMENTATIONKEY: "95c3d0e3-d463-4a8d-a496-f22e9806bb8b",
+  WEBSITE_HOSTNAME: "localhost",
+  BASIC_AUTH_USERNAME: "testUser",
+  BASIC_AUTH_PASSWORD: "testPassword",
 })
