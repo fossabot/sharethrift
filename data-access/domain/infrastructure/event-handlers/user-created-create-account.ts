67b7aaafdb9dd3d89ad4b7d110f7829dcc9f6a01
Patrick Gidich <patrick@simnova.com>
2021-10-26 00:16:21 -0400

    repo cleanup

--- data-access/domain/infrastructure/event-handlers/user-created-create-account.ts
+++ data-access/domain/infrastructure/event-handlers/user-created-create-account.ts
@@ -1,21 +1,17 @@
 import { NodeEventBus } from '../events/node-event-bus';
-import { InProcEventBus } from '../events/in-proc-event-bus';
-
 import { UserCreatedEvent } from '../../events/user-created';
-import { MongoUnitOfWork } from '../persistance/mongo-unit-of-work';
-import { AccountModel } from '../../../infrastructure/data-sources/cosmos-db/models/account';
-import { AccountConverter } from '../persistance/repositories/mongo-account-converter';
-import { MongoAccountRepository } from '../persistance/repositories/mongo-account-repository';
+import { AccountUnitOfWork } from '../persistance/repositories';
 
 export default () => { NodeEventBus.register(UserCreatedEvent, async (payload) => {
+
   console.log(`UserCreatedEvent -> CreateAccount Handler - Called with Payload: ${JSON.stringify(payload)} and UserId: ${payload.userId}`);
-  var uow  = new MongoUnitOfWork(InProcEventBus,NodeEventBus, AccountModel, new AccountConverter(), MongoAccountRepository)
-  await uow.withTransaction(async (repo) => {
+  
+  await AccountUnitOfWork.withTransaction(async (repo) => {
     var accounts = await repo.getByUserId(payload.userId);
     if(!accounts || accounts.length == 0) {
       var newAccount = repo.getNewInstance(payload.userId);
       await repo.save(newAccount);
     }
   });
-  console.log("hello world");
+
 })};
--- data-access/domain/infrastructure/event-handlers/user-created-create-category.ts
+++ data-access/domain/infrastructure/event-handlers/user-created-create-category.ts
@@ -1,13 +1,12 @@
 import { NodeEventBus } from '../events/node-event-bus';
 import { UserCreatedEvent } from '../../events/user-created';
-
 import { CategoryUnitOfWork } from '../persistance/repositories';
 
 export default () => { NodeEventBus.register(UserCreatedEvent, async (payload) => {
 
   console.log(`UserCreatedEvent -> CreateCategory Handler - Called with Payload: ${JSON.stringify(payload)} and UserId: ${payload.userId}`);
 
-  CategoryUnitOfWork.withTransaction(async(repo) => {
+  await CategoryUnitOfWork.withTransaction(async(repo) => {
     var newCategory = repo.getNewInstance();
     newCategory.name = payload.userId;
     repo.save(newCategory);
