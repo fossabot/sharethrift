14826678cf41511f86108ceb270d0e6457409a69
Patrick Gidich <patrick@simnova.com>
2021-10-27 23:11:33 -0400

    updating query side to use adapters

--- data-access/graphql/data-sources/cosmos-db/categories.ts
+++ data-access/graphql/data-sources/cosmos-db/categories.ts
@@ -1,20 +1,20 @@
 import { MongoDataSource } from 'apollo-datasource-mongodb';
 import * as Category from '../../../infrastructure/data-sources/cosmos-db/models/category';
 import {Context} from '../../context';
-import { CategoryDomainAdapter } from '../../../domain/infrastructure/persistance/adapters/category-domain-adapter';
+import { CategoryConverter } from '../../../domain/infrastructure/persistance/adapters/category-domain-adapter';
 import { CategoryEntityReference } from '../../../domain/contexts/listing/category';
 
 export default class Categories extends MongoDataSource<Category.Category, Context> {
 
   async getCategory(categoryId : string): Promise<CategoryEntityReference> {
-    return new CategoryDomainAdapter(await this?.findOneById(categoryId));
+    return (new CategoryConverter).toDomain(await this?.findOneById(categoryId));
   }
   
   async getCategories(): Promise<CategoryEntityReference[]> {
     return (await this.model
       .find({})
       .exec())
-      .map(category => new CategoryDomainAdapter(category));
+      .map(category => (new CategoryConverter).toDomain(category));
   } 
    
 }
--- data-access/graphql/data-sources/cosmos-db/users.ts
+++ data-access/graphql/data-sources/cosmos-db/users.ts
@@ -1,13 +1,13 @@
 import { MongoDataSource } from 'apollo-datasource-mongodb';
 import * as User from '../../../infrastructure/data-sources/cosmos-db/models/user';
 import {Context} from '../../context';
-import { UserDomainAdapter } from '../../../domain/infrastructure/persistance/adapters/user-domain-adapter';
+import { UserConverter } from '../../../domain/infrastructure/persistance/adapters/user-domain-adapter';
 import { UserEntityReference } from '../../../domain/contexts/user/user';
 
 export default class Users extends MongoDataSource<User.User, Context> {
   
   async getUser(userId : string): Promise<UserEntityReference> {
-    return new UserDomainAdapter(await this.findOneById(userId));
+    return (new UserConverter).toDomain(await this.findOneById(userId));
   }
 
   async getUsers(): Promise<UserEntityReference[]> {
@@ -15,7 +15,7 @@ export default class Users extends MongoDataSource<User.User, Context> {
     return (await this.model
       .find({})
       .exec())
-      .map((user: User.User) => new UserDomainAdapter(user));
+      .map((user: User.User) => (new UserConverter).toDomain(user));
   }
   
 }
